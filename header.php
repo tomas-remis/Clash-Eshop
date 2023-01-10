6366fd2a78b64b609ff5a421d1f1d9e6f578a9b6
Tomáš Remiš <23260946+tomas-remis@users.noreply.github.com>
2023-01-10 21:26:22 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -2,17 +2,23 @@
 $is_in_verified = gettype(strpos($_SERVER["REQUEST_URI"], "unverified")) == "boolean";
 function add_header_elements(){
     GLOBAL $is_in_verified;
+
     if($is_in_verified){
-        return "<table>
+        if(!isset($_SESSION['cart'])){
+            $_SESSION['cart'] = array();
+        }
+        return  "<nav id='header'><table>
             <tr>
                 <td><a href='home.php'>Home</a></td>
                 <td><a href='browse.php'>Browse cards</a></td>
                 <td><a href='orders.php'>My orders</a></td>
                 <td><a href='logout.php'>Log out</a></td>
             </tr>
-        </table>";
+        </table>
+        </nav>
+        <a id='cart' href='cart.php'><img alt='cart' src='/clash/images/cart.png'></a>";
     }
-    return "";
+    return "<nav id='header'></nav>";
 }
 
 session_set_cookie_params(7200,'/');
@@ -31,9 +37,7 @@ function load_head(){
 }
 
 function load_body(){
-    $data = '<nav id="header">
-        '.add_header_elements().'
-    </nav>';
+    $data = add_header_elements();
     return $data;
 }
 ?>
